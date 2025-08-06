92168467bbaa235d3c6149516b2a78982557524e
Nathan Hettige <nathan.hettige@gmail.com>
2025-08-06 11:35:21 +1000

    feat: add axios block and cli command

--- app/page.tsx
+++ app/page.tsx
@@ -20,6 +20,18 @@ export default function Home() {
         <div className="flex flex-col gap-4 border rounded-lg p-4 min-h-[450px] relative">
           <div className="flex items-center justify-between">
             <h2 className="text-sm text-muted-foreground sm:pl-3">
+              Sample Axios API Setup
+            </h2>
+            <OpenInV0Button name="axios" className="w-fit" />
+          </div>
+          <div className="flex items-center justify-center min-h-[400px] relative">
+            <p>No preview</p>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-4 border rounded-lg p-4 min-h-[450px] relative">
+          <div className="flex items-center justify-between">
+            <h2 className="text-sm text-muted-foreground sm:pl-3">
               A simple hello world component
             </h2>
             <OpenInV0Button name="hello-world" className="w-fit" />
@@ -66,5 +78,5 @@ export default function Home() {
         </div>
       </main>
     </div>
-  )
+  );
 }
